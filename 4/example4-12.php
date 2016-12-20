a941d2742dcced11ddf59cbec37ffd58087e6cef
learndev2016 <temur-nauruzov@yandex.ru>
2016-12-20 04:03:28 +0300

    Update example4-12.php

--- 4/example4-12.php
+++ 4/example4-12.php
@@ -1,5 +1,8 @@
 <?php
   $month = "March";
 
-  if ($month == "March") echo "It's springtime";
+  if ($month == "March" || $month == "April" || $month == "May") echo "It's springtime";
+  else if($month == "June" || $month == "July" || $month == "August") echo "It's summertime";
+  else if($month == "September" || $month == "October" || $month == "November") echo "It's autumntime";
+  else if($month == "December" || $month == "Junuary" || $month == "Februry") echo "It's wintertime";
 ?>
