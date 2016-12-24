0fe3658f7aefd8a87938d170e1e555cc87d01fe5
learndev2016 <temur-nauruzov@yandex.ru>
2016-12-25 00:05:59 +0300

    Update setPass.php

--- MySite/modules/setPass.php
+++ MySite/modules/setPass.php
@@ -2,20 +2,19 @@
 	include_once('../include/function.php');
 	
 	$username = 'admin';
-	$oldpassword = get_data("SELECT sha_pass_hash FROM `".$GLOBALS['db_auth']."`.`account` WHERE `username`='$username'");;
-	$oldpassword[$username] = 'admin';
+	$password = get_pass($password);
+	$password['new'] = 'admin';
 	$newpassword = 'admin1';
 	
-	$oldpassword[$username] = sha_pass($username,$oldpassword[$username]);
-	//print_info($oldpassword);
+	$password['new'] = sha_pass($username,$password['new']);
+	//print_info($password);
 	
-	if($oldpassword['sha_pass_hash'] != $oldpassword[$username]){
+	if($password['sha_pass_hash'] != $password['new']){
 		echo "Invalid old pass";
 	}elseif(preg_match('/[a-zA-Z0-9_-]/',$newpassword)){
 		if(strlen($newpassword) >=4){
 			$newpassword = sha_pass($username,$newpassword);
 			get_data("UPDATE `".$GLOBALS['db_auth']."`.`account` SET `sha_pass_hash`='$newpassword',`v`=NULL,`s`=NULL WHERE `username`='$username'");
-			//get_data("UPDATE `".$GLOBALS['db_auth']."`.`account` SET ` WHERE `username`='$username'");
 			echo "Пароль успешно изменен";
 		}else{
 			echo 'Пароль должен содержать минимум 4 символа';
